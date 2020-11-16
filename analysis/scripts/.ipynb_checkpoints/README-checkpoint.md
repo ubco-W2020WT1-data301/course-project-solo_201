d4928a5b4e48a636c4169c1e73202709e9141f45
harshg03 <harsh.gill@hotmail.com>
2020-11-16 10:16:24 -0800

    Milestone 2 notebook with EDA, updated README's and project_function file

---
+++ analysis/scripts/.ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,2 @@
+### Information about Folder
+>This folder contains all of the **functions** associated with milestone 2 notebook for our data analysis.
---
+++ analysis/scripts/.ipynb_checkpoints/project_function-checkpoint.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def loadWine(url):
+#loading dataset, no need to use dropna due to no na values
+        whitedf = (
+            pd.read_excel(url)
+           
+        )
+    
+        return whitedf
+#removes outliers using IQR
+def removeOut(whitedf):
+    Q1 = whitedf.quantile(0.25)
+    Q3 = whitedf.quantile(0.75)
+    IQR = Q3 - Q1
+    whitedfout = whitedf[~((whitedf < (Q1 - 1.5 * IQR)) |(whitedf > (Q3 + 1.5 * IQR))).any(axis=1)]
+    return whitedfout
+
